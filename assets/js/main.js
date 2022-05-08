4d541faeeb242c6bdd49136a1f5e63f4ccc2837a
nazmul128 <shafayat536@gmail.com>
2022-05-08 16:05:56 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.dicover-carousel-area').owlCarousel({ 
+        loop:true,
+        margin:10,
+        nav:false,
+        items: 1,
+        autoplay: true
+    })
+})
+
+jQuery(document).ready(function($) {
+    $('.counter-up').counterUp({
+        delay: 10,
+        time: 3000
+    });
+});
+
